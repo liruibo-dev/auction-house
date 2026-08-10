abe462716803fccf9b0e7ff9736fe10c58a3fda5
li-auto-test <1696760723@qq.com>
2026-08-10 09:42:33 +0800

    初始化项目框架

---
+++ src/main/java/com/auctionhouse/SuctionHouseApplication.java
@@ -0,0 +1,13 @@
+package com.auctionhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuctionHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuctionHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auctionhouse/SuctionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctionhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuctionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
